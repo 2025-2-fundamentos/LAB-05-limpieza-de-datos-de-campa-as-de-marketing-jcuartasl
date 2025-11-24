9cd41b001c59c31a0c4c430f37c189ed9a25db98
jcuartasl <jcuartasl@unal.edu.co>
2025-11-24 12:41:58 -0500

    finale

--- homework/homework.py
+++ homework/homework.py
@@ -50,6 +50,129 @@ def clean_campaign_data():
 
     """
 
+    import os, glob
+    import pandas as pd
+
+    df_clients = pd.DataFrame(
+        columns=[
+            "client_id",
+            "age",
+            "job",
+            "marital",
+            "education",
+            "credit_default",
+            "mortgage",
+        ]
+    )
+    df_campaign = pd.DataFrame(
+        columns=[
+            "client_id",
+            "number_contacts",
+            "contact_duration",
+            "previous_campaign_contacts",
+            "previous_outcome",
+            "campaign_outcome",
+            "last_contact_date",
+        ]
+    )
+    df_economics = pd.DataFrame(
+        columns=[
+            "client_id",
+            "cons_price_idx",
+            "euribor_three_months",
+        ]
+    )
+
+
+    def proccess_client_data(data):
+        aux_df = data.copy()
+        aux_df["job"] = aux_df["job"].str.replace(".", "", regex=False)
+        aux_df["job"] = aux_df["job"].str.replace("-", "_", regex=False)
+
+        aux_df["education"] = aux_df["education"].str.replace(".", "_", regex=False)
+        aux_df["education"] = aux_df["education"].replace("unknown", pd.NA)
+
+        aux_df["credit_default"] = aux_df["credit_default"].apply(lambda x: 1 if x == "yes" else 0)
+
+        aux_df["mortgage"] = aux_df["mortgage"].apply(lambda x: 1 if x == "yes" else 0)
+
+        return aux_df
+
+    def build_client_csv(base, data_frame):
+        client_data = data_frame[
+            [
+                "client_id",
+                "age",
+                "job",
+                "marital",
+                "education",
+                "credit_default",
+                "mortgage",
+            ]
+        ]
+        return pd.concat([base, proccess_client_data(client_data)], ignore_index=True)
+
+    def proccess_campaign_data(data):
+        aux_df = data.copy()
+        aux_df["previous_outcome"] = aux_df["previous_outcome"].apply(lambda x: 1 if x == "success" else 0)
+        aux_df["campaign_outcome"] = aux_df["campaign_outcome"].apply(lambda x: 1 if x == "yes" else 0)
+        aux_df["last_contact_date"] = ("2022-" + aux_df["month"].astype(str) + "-" + aux_df["day"].astype(str)).astype("datetime64[ns]")
+        
+        aux_df = aux_df.drop(columns=["day", "month"])
+
+        return aux_df
+
+    def build_campaign_csv(base, data_frame):
+        campaign_data = data_frame[
+            [
+                "client_id",
+                "number_contacts",
+                "contact_duration",
+                "previous_campaign_contacts",
+                "previous_outcome",
+                "campaign_outcome",
+                "day",
+                "month",
+            
+            ]
+        ]
+
+        return pd.concat([base, proccess_campaign_data(campaign_data)], ignore_index=True)
+
+
+    def proccess_economics_data(data):
+        aux_df = data.copy()
+        return aux_df
+
+    def build_economics_csv(base, data_frame):
+        economics_data = data_frame[
+            [
+                "client_id",
+                "cons_price_idx",
+                "euribor_three_months",
+            ]
+        ]
+
+        return pd.concat([base, proccess_economics_data(economics_data)], ignore_index=True)
+
+    for file in glob.glob("./files/input/*.csv.zip"):
+        df = pd.read_csv(file, compression="zip", sep=",")
+
+        df_clients = build_client_csv(df_clients, df)
+        df_campaign = build_campaign_csv(df_campaign, df)
+        df_economics = build_economics_csv(df_economics, df)
+
+    if os.path.exists("files/output/"):
+        for file in glob.glob(f"files/output/*"):
+            os.remove(file)
+    else:
+        os.makedirs("files/output")
+
+    df_clients.to_csv("files/output/client.csv", index=False)
+    df_campaign.to_csv("files/output/campaign.csv", index=False)
+    df_economics.to_csv("files/output/economics.csv", index=False)
+
+
     return
 
 
